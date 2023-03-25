e0a1bb441268c62c5d8ea03064670d5547cf0eee
phw4604 <phw7846@naver.com>
2023-03-25 14:16:20 +0900

    123

---
+++ Project2/Project2/FileName.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+int main(void) {
+
+
+	char name[15];
+	int num1, num2;
+	char major[33];
+	printf("이름 : ");
+	scanf_s("%s", name, sizeof(name));
+	printf("생년월일 : ");
+	scanf_s("%d", &num1);
+	printf("학번 : ");
+	scanf_s("%d", &num2);
+	printf("학과 : ");
+	scanf_s("%s", major, sizeof(major));
+
+	printf("\n이름 : %s", name);
+	printf("\n생년월일 : %d", num1);
+	printf("\n학번 : %d", num2);
+	printf("\n학과 : %s", major);
+
+
+}
---
+++ Project3/Project3/FileName.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+main(void) {
+
+	int sum = 0;
+
+	for (int i = 0; i <= 10; i = i++) {
+		sum += i;
+	}
+
+	printf("sum : %d", sum);
+}
+
+
+
+
+
